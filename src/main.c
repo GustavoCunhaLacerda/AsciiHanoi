b8b646bd15906ed96c5235c003b4b5675a307f17
Gustavo Lacerda <gustavocunhalacerda@gmail.com>
2020-08-28 18:44:03 -0300

    Primeiros teste de gameplay e mecânicas

--- src/main.c
+++ src/main.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include "./RenderizarTorres.h"
+#include "./Torre.h"
 
 int main () {
+    printf("Torre de Hanoi\n");
+
+    short tamTotal = 2;
+
+    Torre* t1 = cria_torre();
+    for (short i = tamTotal; i > 0; i--)
+        push(t1, (2*i)-1);
+    Torre* t2 = cria_torre();
+    Torre* t3 = cria_torre();
+
+    Torre* torres[] = {t1, t2, t3};
+
+    while ( esta_vazia(torres[0]) == 1 || esta_vazia(torres[1]) == 1) {
+        renderizar_torres(tamTotal, vetor_torre(torres[0], tamTotal), vetor_torre(torres[1], tamTotal), vetor_torre(torres[2], tamTotal));
+
+        short fonte, destino;
+        printf("Mover de: ");
+        scanf("%hi", &fonte);
+        printf("Para: ");
+        scanf("%hi", &destino);
+
+        push(torres[destino-1], pop(torres[fonte-1]));
+    }
+    printf("Parabéns, você conseguiu !\n\n");
+    renderizar_torres(tamTotal, vetor_torre(torres[0], tamTotal), vetor_torre(torres[1], tamTotal), vetor_torre(torres[2], tamTotal));
     
-    int tamTotal = 4;
 
-    int v_torre1[] = {0, 3, 5, 7}; 
-    int v_torre2[] = {0, 0, 0, 3}; 
-    int v_torre3[] = {0, 0, 0, 1}; 
 
-    printf("TORRE DE HANOI !!\n");
-
-    renderizar_torres(tamTotal, v_torre1, v_torre2, v_torre3);
-    return 0;
+    liberar_torre(t1);
+    liberar_torre(t2);
+    liberar_torre(t3);
 }
