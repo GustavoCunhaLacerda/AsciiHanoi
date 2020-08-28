b1d1bf2dd9d3e48543b0503f0ed01be732c743aa
Gustavo Lacerda <gustavocunhalacerda@gmail.com>
2020-08-28 15:12:02 -0300

    Reajuste de variáveis

--- src/RenderizarTorres.c
+++ src/RenderizarTorres.c
@@ -17,14 +17,13 @@ void renderizar_linha_torre(int tam_disco, int linha, int tamTotal) {
 }
 
 void exibe_torres() {
-    int tamTotal, linha, espaco, simbolo;
-    tamTotal = 4;
+    int tamTotal = 4;
 
     int torre1[] = {0, 3, 5, 7}; 
     int torre2[] = {0, 0, 0, 3}; 
     int torre3[] = {0, 0, 0, 1}; 
 
-    for (linha = 0; linha < tamTotal; linha++) {
+    for (int linha = 0; linha < tamTotal; linha++) {
         Torre1: renderizar_linha_torre(torre1[linha], linha, tamTotal);
         Torre2: renderizar_linha_torre(torre2[linha], linha, tamTotal);
         Torre3: renderizar_linha_torre(torre3[linha], linha, tamTotal);
@@ -34,8 +33,6 @@ void exibe_torres() {
 }
 
 int main () {
-
     exibe_torres();
-
     return 0;
 }
