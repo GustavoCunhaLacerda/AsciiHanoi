e0933bb0bf3627ecf11c1b17f0e686ffe96f9de6
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2020-08-31 18:35:46 -0300

    Refatoração de Implementação das Torres e Discos

--- includes/TorreEDisco.h
+++ includes/TorreEDisco.h
@@ -12,25 +12,31 @@ typedef struct torre Torre;
 Torre* cria_torre();
 
 // Libera a memória alocada pela torre
-void liberar_torre(Torre* t);
+void libera_torre(Torre* t);
 
 // Verifica se a torre está vazia
 short esta_vazia(Torre* t);
 
 // Insere disco no topo da torre
-void push(Torre* t, short qte_discos);
+void push(Torre* t, Disco* disco);
 
 // Retorna e remove o disco do topo da torre
-short pop(Torre* t);
+Disco* pop(Torre* t);
 
 // Retorna o topo da torre sem remover
-short peek(Torre* t);
+Disco* peek(Torre* t);
 
 // Retorna um vetor formatado para renderização da torre
-short* vetor_torre(Torre* t, short tam_total);
+short* vetor_torre(Torre* t, short tamVetorFinal);
 
 // 
 // Disco
 // 
 
 // Cria disco
+Disco* cria_disco(short tam);
+
+// Libera disco
+void libera_disco(Disco* d);
+
+
---
+++ src/TorreEDisco.c
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include "../includes/TorreEDisco.h"
+
+// Tipos exportados
+struct torre
+{
+    short qte_discos;
+    Disco* topo;
+};
+
+struct disco
+{
+    short tamanho;
+    Disco* prox;
+};
+
+// Funções exportadas
+
+//
+// Torre
+//
+
+// Criar torre
+Torre* cria_torre()
+{
+    Torre* t = malloc(sizeof(Torre));
+    t->topo = NULL;
+    t->qte_discos = 0;
+    return t;
+} 
+
+// Libera a memória alocada pela torre
+void libera_torre(Torre* t) 
+{
+    free(t);
+}
+
+// Verifica se a torre está vazia
+short esta_vazia(Torre* t)
+{
+    if (t->qte_discos == 0)
+        return 0;
+    else
+        return 1;
+}
+
+// Insere disco no topo da torre
+void push(Torre* t, Disco* d)
+{
+    // Disco* nova = malloc(sizeof(Disco));
+    // nova->tamanho = qte_discos;
+    if (t->topo == NULL)
+    {
+        d->prox = NULL;
+        t->topo = d;
+    } 
+    else
+    {
+        d->prox = t->topo;
+        t->topo = d;
+    }
+    t->qte_discos += 1;
+}
+
+// Retorna e remove o disco do topo da torre
+Disco* pop(Torre* t)
+{
+    Disco* dt = t->topo;
+    t->topo = dt->prox;
+    dt->prox = NULL;
+    t->qte_discos -= 1;
+
+    return dt;
+}
+
+// Retorna o topo da torre sem remover
+Disco* peek(Torre* t)
+{
+    return t->topo;
+}
+
+// Retorna um vetor formatado para renderização da torre
+short* vetor_torre(Torre* t, short tamVetorFinal)
+{
+    short* v_torre = malloc(sizeof(short)*(tamVetorFinal));
+    Disco* d = t->topo;
+    for (short i = 0; i < tamVetorFinal; i++)
+    {
+        if(tamVetorFinal - i > t->qte_discos)
+            v_torre[i] = 0;
+        else
+        {
+            v_torre[i] = d->tamanho;
+            d = d->prox; 
+        }
+    }
+    return v_torre;
+}
+
+// 
+// Disco
+// 
+
+// Cria disco
+Disco* cria_disco(short tam)
+{
+    Disco* d = malloc(sizeof(Disco));
+    d->tamanho = tam;
+    d->prox = NULL;
+
+    return d;
+}
+
+// Libera disco
+void libera_disco(Disco* d)
+{
+    free(d);
+}
