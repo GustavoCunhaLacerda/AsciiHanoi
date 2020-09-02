a6aa20fa5e6570122e413146bfcda315de084623
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2020-09-02 09:59:15 -0300

    Ajustes de renderização

--- includes/RenderizarTorres.h
+++ includes/RenderizarTorres.h
@@ -1,2 +1,10 @@
+// 
+// Renderização das torres
+// 
+
+// Funções exportadas
+
+// Imprime 3 torres a partir de um tamanho total de discos no "tabuleiro"
+// e 3 vetores que representam as torres.
 void renderizar_torres(short tamTotal, short v_torre1[], short v_torre2[], short v_torre3[]);
 
--- src/RenderizarTorres.c
+++ src/RenderizarTorres.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void renderizar_linha_torre(short tam_disco, short linha, short tamTotal) {
+// Função que printa determinada linha da torre
+void renderizar_linha_torre(short tamDisco, short linha, short tamTotal) {
     // exibindo espaços
-    if (tam_disco == 0) putchar(' ');
-    for (short espaco = 0; espaco < tamTotal - (1 + (tam_disco >> 1)); espaco++)
+    if (tamDisco == 0) putchar(' ');
+    for (short espaco = 0; espaco < tamTotal - (1 + (tamDisco >> 1)); espaco++)
         putchar(' ');
     // exibindo simbolos
-    for (short simbolo = 0; simbolo < tam_disco; simbolo++)
+    for (short simbolo = 0; simbolo < tamDisco; simbolo++)
         putchar('*');
     // exibindo espaços
-    for (short espaco = 0; espaco < tamTotal - (1 + (tam_disco >> 1)); espaco++)
+    for (short espaco = 0; espaco < tamTotal - (1 + (tamDisco >> 1)); espaco++)
         putchar(' ');
-
     putchar(' ');
 }
 
-void renderizar_label(short tamTotal, char label_torre) {
+// Função que printa a label da torre de acrdo com o tamanho total
+// A label deve ter apenas um caractere descritivo ('1', '2' ou 'A', 'B')
+void renderizar_label(short tamTotal, char labelTorre) {
     for (short i = 0; i < tamTotal-1; i++)
         putchar('-');
-    putchar(label_torre);
+    putchar(labelTorre);
     for (short i = 0; i < tamTotal-1; i++)
         putchar('-');
     putchar(' ');
 } 
 
-void renderizar_torres(short tamTotal, short v_torre1[], short v_torre2[], short v_torre3[]) {
-   
+// Função que printa o tabuleiro levando em conta o tamanho total das torres.
+// Cada torre é renderizada com base em um vetor de tamanho igual ao tamTotal (espaços vazios de torres devem possuir zero para manter o padrão)
+// Abaixo da torre é costruído a label de forma dinâmica
+void renderizar_torres(short tamTotal, short* v_torre1, short* v_torre2, short* v_torre3) {
+    
+    // Torres
     for (short linha = 0; linha < tamTotal; linha++) {
         Torre1: renderizar_linha_torre(v_torre1[linha], linha, tamTotal);
         Torre2: renderizar_linha_torre(v_torre2[linha], linha, tamTotal);
@@ -34,13 +40,17 @@ void renderizar_torres(short tamTotal, short v_torre1[], short v_torre2[], short
         putchar('\n');
     }
 
+    // Labels
     renderizar_label(tamTotal, '1');
     renderizar_label(tamTotal, '2');
     renderizar_label(tamTotal, '3');
     putchar('\n');
 
+    // Liberação dos vetores
     free(v_torre1);
     free(v_torre2);
     free(v_torre3);
 }
+
+
 
