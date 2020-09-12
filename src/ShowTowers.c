dc1a0aa1d4154740faafdd15f5c3af236aa75a27
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2020-09-12 00:23:40 -0300

    show_tower documentation

--- src/ShowTowers.c
+++ src/ShowTowers.c
@@ -36,9 +36,16 @@ void show_tower_label(short totalDiscs, char towerLabel) {
     putchar(' ');
 } 
 
-// Função que printa o tabuleiro levando em conta o tamanho total das torres.
-// Cada torre é renderizada com base em um vetor de tamanho igual ao totalDiscs (espaços vazios de torres devem possuir zero para manter o padrão)
-// Abaixo da torre é costruído a label de forma dinâmica
+/**
+ * Função que printa o tabuleiro levando em conta a quantidade total de discos.
+ * Cada torre é renderizada com base em um vetor de tamanho igual ao totalDiscs.
+ * Espaços vazios dos vetores torres devem possuir zero para manter o padrão.
+ * Abaixo da torre é costruído a label de forma dinâmica.
+ * @param totalDiscs Numero total de discos em jogo.
+ * @param v_tower1 Vetor formatado formado a partir do estado atual da torre 1.
+ * @param v_tower2 Vetor formatado formado a partir do estado atual da torre 2.
+ * @param v_tower3 Vetor formatado formado a partir do estado atual da torre 3.
+ */
 void show_tower(short totalDiscs, short* v_tower1, short* v_tower2, short* v_tower3) {
     
     // Torres
