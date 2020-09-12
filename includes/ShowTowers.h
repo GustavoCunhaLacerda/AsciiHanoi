9980b520e45c734d4c68a8bedff591652914d9b0
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2020-09-12 00:24:33 -0300

    documentation in ShowTowers.h file

--- includes/ShowTowers.h
+++ includes/ShowTowers.h
@@ -1,11 +1,14 @@
 #pragma once
-// 
-// Renderização das torres
-// 
 
-// Funções exportadas
-
-// Imprime 3 torres a partir de um tamanho total de discos no "tabuleiro"
-// e 3 vetores que representam as torres.
+/**
+ * Função que printa o tabuleiro levando em conta a quantidade total de discos.
+ * Cada torre é renderizada com base em um vetor de tamanho igual ao totalDiscs.
+ * Espaços vazios dos vetores torres devem possuir zero para manter o padrão.
+ * Abaixo da torre é costruído a label de forma dinâmica.
+ * @param totalDiscs Numero total de discos em jogo.
+ * @param v_tower1 Vetor formatado formado a partir do estado atual da torre 1.
+ * @param v_tower2 Vetor formatado formado a partir do estado atual da torre 2.
+ * @param v_tower3 Vetor formatado formado a partir do estado atual da torre 3.
+ */
 void show_tower(short totalDiscs, short v_tower1[], short v_tower2[], short v_tower3[]);
 
